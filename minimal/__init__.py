6344dba85329966f4f1ae1c194224a7918213e5e
ivan-math <142260687+ivan-math@users.noreply.github.com>
2023-11-22 14:41:19 -0600

    Update __init__.py

--- minimal/__init__.py
+++ minimal/__init__.py
@@ -6,3 +6,13 @@ def my_sum(x, y):
 def my_mul(x, y):
     """A function that multiply. """
     return x*y
+
+
+def numeros_fibonacci(n):
+    """Funcion que da los numeros de Fibonacci hasta el numero n"""
+    fibs = []
+    a, b = 0, 1
+    while a < n:
+        fibs.append(a)
+        a, b = b, a+b
+    return fibs
